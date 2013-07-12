21412bb9cc7b4368019d29e3942bc82d9e565da3
Ubuntu <ubuntu@ip-172-31-18-177.us-west-2.compute.internal>
2013-07-12 04:50:56 +0000

    Modified with include web.js

--- web.js
+++ web.js
@@ -1,7 +1,8 @@
 var express = require('express');
+var fs = require('fs')
 
 var app = express.createServer(express.logger());
-var bf = new Buffer(fs.readFileSync('index.html'))
+var bf = new Buffer(fs.readFileSync('index.html'));
 app.get('/', function(request, response) {
   response.send(bf.toString());
 
