55f08b47c739d3f724c2ca612823a04f94d68eec
Ubuntu <ubuntu@ip-172-31-18-177.us-west-2.compute.internal>
2013-07-12 04:54:35 +0000

    Modified with include scob web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 var bf = new Buffer(fs.readFileSync('index.html'));
 app.get('/', function(request, response) {
   response.send(bf.toString());
-
+});
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
